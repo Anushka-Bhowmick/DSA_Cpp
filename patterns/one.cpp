e9b25daf9b8f47192451c116ac9171880e3006c3
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-08-11 20:13:43 +0530

    added a new folder for patterns problems

---
+++ patterns/one.cpp
@@ -0,0 +1,22 @@
+  #include<bits/stdc++.h>
+using namespace std;
+void print1(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int t;
+    cout<<"enter t: ";
+    cin>>t;
+     int n;
+    for(int i=0;i<t;i++){
+        cout<<"Enter n: ";
+        cin >> n;
+        print1(n);
+    }
+    
+}
