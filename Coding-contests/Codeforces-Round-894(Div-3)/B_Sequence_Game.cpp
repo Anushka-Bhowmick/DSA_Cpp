926ce3b095f7a712f96417856b0be768da0a239b
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2023-08-25 23:21:24 +0530

    Create B_Sequence_Game.cpp

---
+++ Coding-contests/Codeforces-Round-894(Div-3)/B_Sequence_Game.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <map>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<string> vs;
+typedef vector<vector<ll>> vvll;
+typedef map<ll, bool> mllb;
+typedef unordered_set<char> ucst;
+typedef unordered_map<ll, bool> umllb;
+#define PI 3.1415926535897932384626
+#define pb push_back
+#define mp make_pair
+const ll mod = 10e9 + 7;
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    vll arr(n);
+    vll ans;
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+
+    ans.push_back(arr[0]);
+
+    for(int i=1;i<n;i++) {
+        ans.push_back(arr[i]);
+        if(arr[i]<arr[i-1]) {
+            ans.push_back(arr[i]);
+        }
+    }
+
+    cout<<ans.size()<<endl;
+    for(auto x: ans) {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--)
+    {
+        /*if(solve())
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;*/
+        solve();
+        // cout<<solve()<<endl;
+    }
+
+    return 0;
+}
